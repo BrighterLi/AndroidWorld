59c5a442866d6d0a82654e804d1427fba83b4e8f
liming <287055586@qq.com>
2020-02-12 09:42:46 +0800

    解决PopupWindow问题，并创建Authority权限工程

---
+++ oauthority/src/main/java/com/xiaoming/oauthority/MainActivity.java
@@ -0,0 +1,14 @@
+package com.xiaoming.oauthority;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//权限
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
