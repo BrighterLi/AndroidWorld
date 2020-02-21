ed58e9d1ae6c62a8a3ec5c788713f71244800161
liming <287055586@qq.com>
2020-02-21 08:18:55 +0800

    Loading 原生控件ProgressDialog的使用

--- widgetloading/readme.txt
+++ widgetloading/readme.txt
@@ -1,2 +1,4 @@
-1 原生Loading控件：ProgressDialog
-2 自定义Loading控件
+原生Loading控件：ProgressDialog android.app.ProgressDialog
+   普通的圆形进度条对话框
+   普通的条形进度条对话框，不更新进度
+   普通的条形进度条对话框，更新进度
--- widgetloading/src/main/java/com/xiaoming/widgetloading/MainActivity.java
+++ widgetloading/src/main/java/com/xiaoming/widgetloading/MainActivity.java
@@ -1,13 +1,45 @@
 package com.xiaoming.widgetloading;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+//原生的Loading控件 ProgressDialog
+//https://blog.csdn.net/lpCrazyBoy/article/details/80758619
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button btNormalDialog;
+    private Button btLinearDialog;
+    private Button btLinearDialogUpdate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btNormalDialog = findViewById(R.id.bt_normal_dialog);
+        btLinearDialog = findViewById(R.id.bt_linear_dialog);
+        btLinearDialogUpdate = findViewById(R.id.bt_linear_dialog_update);
+
+        //注册监听
+        btNormalDialog.setOnClickListener(this);
+        btLinearDialog.setOnClickListener(this);
+        btLinearDialogUpdate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bt_normal_dialog:
+                startActivity(new Intent(MainActivity.this, NormalDialogActivity.class));
+                break;
+            case R.id.bt_linear_dialog:
+                startActivity(new Intent(MainActivity.this,LinearDialogActivity.class));
+                break;
+            case R.id.bt_linear_dialog_update:
+                startActivity(new Intent(MainActivity.this, LinearDialogUpdateActivity.class));
+        }
     }
 }
