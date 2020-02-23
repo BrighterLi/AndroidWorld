6c5b71f9b143864bfe3a5cb2861d0c7de13a20ee
liming <287055586@qq.com>
2020-02-23 21:56:49 +0800

    ViewPager 创建

--- 001viewcustom/src/main/java/com/xiaoming/a001viewcustom/CircleImageView.java
+++ 001viewcustom/src/main/java/com/xiaoming/a001viewcustom/CircleImageView.java
@@ -12,7 +12,7 @@ import android.util.AttributeSet;
 import android.view.View;
 
 public class CircleImageView extends View {
-    private Paint mPaint;
+    private Paint mPaint;  //画笔
     private Bitmap imageBitmap;
     private float circleRadio;
 
@@ -23,10 +23,11 @@ public class CircleImageView extends View {
 
     public CircleImageView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        mPaint = new Paint();
-        imageBitmap = BitmapFactory.decodeResource(getResources(),R.drawable.square_image);
+        mPaint = new Paint(); //画笔
+        imageBitmap = BitmapFactory.decodeResource(getResources(),R.mipmap.square_image);
     }
 
+    //重写onDraw方法
     @Override
     protected void onDraw(Canvas canvas) {
         canvas.drawBitmap(getCircle(imageBitmap),0,0,mPaint);
@@ -35,7 +36,7 @@ public class CircleImageView extends View {
     //将bitmap转换成圆形bitmap
     private Bitmap getCircle(Bitmap bitmap) {
         //circleRadio图形图片的半径
-        float circleRadio = bitmap.getWidth()/2;
+        circleRadio = bitmap.getWidth()/2;
         //创建一张新的bitmap,跟传入图片一样宽的正方形bitmap
         Bitmap b = Bitmap.createBitmap(bitmap.getWidth(),bitmap.getWidth(), Bitmap.Config.ARGB_8888);
         //初始化画布，并将刚才创建的bitmap给这个画布，让画布在这bitmap上面
