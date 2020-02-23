20bcda6b8b8940ff265a7090ee7df916ccaafe28
liming <287055586@qq.com>
2020-02-23 10:55:08 +0800

    ViewCustom 自定义View 将图片变成圆形

---
+++ 001viewcustom/src/main/java/com/xiaoming/a001viewcustom/CircleImageViewActivity.java
@@ -0,0 +1,18 @@
+package com.xiaoming.a001viewcustom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//将图片变成圆形
+//https://blog.csdn.net/wsyizmao/article/details/78491422
+//第一步，创建你的自定义类，让它继承View类，并重写构造方法。
+//第二步，重写onDraw()方法。
+public class CircleImageViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_circle_image_view);
+    }
+}
+
