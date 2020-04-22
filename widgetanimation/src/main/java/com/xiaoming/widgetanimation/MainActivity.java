f64128b1ee868a11ecf7b282bca7e74201b9a507
brightli <brightli@lexin.com>
2020-04-22 22:02:15 +0800

    TextView 设置动画

--- widgetanimation/src/main/java/com/xiaoming/widgetanimation/MainActivity.java
+++ widgetanimation/src/main/java/com/xiaoming/widgetanimation/MainActivity.java
@@ -1,13 +1,37 @@
 package com.xiaoming.widgetanimation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+
+
+import com.xiaoming.widgetanimation.textview.TextViewActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button btnTextViewAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        btnTextViewAnim = findViewById(R.id.bt_textview_anim);
+        btnTextViewAnim.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.bt_textview_anim:
+                startActivity(new Intent(MainActivity.this, TextViewActivity.class));
+                break;
+        }
     }
 }
