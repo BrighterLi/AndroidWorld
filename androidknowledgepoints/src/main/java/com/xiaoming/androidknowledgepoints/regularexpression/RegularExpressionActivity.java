14139f368c84ea528ab9153d11ae6dba1ae94baf
brightli <brightli@lexin.com>
2020-06-17 21:56:26 +0800

    正则

---
+++ androidknowledgepoints/src/main/java/com/xiaoming/androidknowledgepoints/regularexpression/RegularExpressionActivity.java
@@ -0,0 +1,15 @@
+package com.xiaoming.androidknowledgepoints.regularexpression;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xiaoming.androidknowledgepoints.R;
+
+public class RegularExpressionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regular_expression);
+    }
+}
