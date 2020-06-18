403daa17e8a901a5466b3a0fde4ba589da23d3a9
brightli <brightli@lexin.com>
2020-06-18 21:58:14 +0800

    正则

--- androidknowledgepoints/src/main/java/com/xiaoming/androidknowledgepoints/regularexpression/RegularExpressionActivity.java
+++ androidknowledgepoints/src/main/java/com/xiaoming/androidknowledgepoints/regularexpression/RegularExpressionActivity.java
@@ -9,6 +9,8 @@ import android.widget.Toast;
 
 import com.xiaoming.androidknowledgepoints.R;
 
+//正则表达式的写法（全面）https://blog.csdn.net/ynd_sg/article/details/79841680
+//读懂正则表达式就这么简单 https://www.cnblogs.com/zery/p/3438845.html
 public class RegularExpressionActivity extends AppCompatActivity {
     private Button mBtRegular;
     private EditText mEtRetular;
@@ -29,7 +31,7 @@ public class RegularExpressionActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String inputStr = mEtRetular.getText().toString();
-                boolean isMatch = RegularUtil.match4(inputStr);
+                boolean isMatch = RegularUtil.match(inputStr);
                 Toast.makeText(RegularExpressionActivity.this, "" + isMatch, Toast.LENGTH_SHORT).show();
             }
         });
