236bee27600e1aa10c4cd656c97af77781761498
brightli <287055586@qq.com>
2020-06-23 08:25:56 +0800

    下拉刷新

---
+++ widget/src/main/java/com/fenqile/widget/pullrefresh/MyListView.java
@@ -0,0 +1,11 @@
+package com.fenqile.widget.pullrefresh;
+
+import android.content.Context;
+import android.widget.ListView;
+
+//Android ListView实现下拉加载功能:https://www.jb51.net/article/121864.htm
+public class MyListView extends ListView {
+    public MyListView(Context context) {
+        super(context);
+    }
+}
