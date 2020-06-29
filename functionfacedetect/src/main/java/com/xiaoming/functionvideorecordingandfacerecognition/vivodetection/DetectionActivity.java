d7d029fe471b4497221ee8520cb6b228686e0efb
brightli <brightli@lexin.com>
2020-06-29 23:10:56 +0800

    functionfacedetect 将录屏和活体检测合在一起

--- functionfacedetect/src/main/java/com/xiaoming/functionvideorecordingandfacerecognition/vivodetection/DetectionActivity.java
+++ functionfacedetect/src/main/java/com/xiaoming/functionvideorecordingandfacerecognition/vivodetection/DetectionActivity.java
@@ -4,7 +4,12 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
@@ -21,8 +26,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.xiaoming.functionvideorecordingandfacerecognition.R;
+import com.xiaoming.functionvideorecordingandfacerecognition.videorecord.RecordActivity;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Calendar;
 import java.util.Iterator;
 import java.util.List;
 
@@ -31,6 +39,8 @@ public class DetectionActivity extends AppCompatActivity implements SurfaceHolde
     private static final String TAG = "DetectionActivity";
     private Button mBtnStartDetect;
     private Button mBtnStopDetect;
+    private Button mBtnStartRecord;
+    private Button mBtnStartVideoPlay;
     private SurfaceView mSurfaceViewDetect;
     public TextView mTvDetectTime;
     public TextView mTvActionState;
@@ -40,8 +50,13 @@ public class DetectionActivity extends AppCompatActivity implements SurfaceHolde
     private VivoDetection mVivoDetection;
 
     private Camera mCamera;
+    private SurfaceHolder mSurfaceHolder; //作用就像一个关于Surface的监听器，提供访问和控制SurfaceView背后的Surface 相关的方法
     private int mCameraWidth = 480;
     private int mCameraHeight = 270;
+    private MediaRecorder mMediaRecorder; //视频录像功能类
+    private String mVideoPath; //视频保存路径
+    private boolean mStartedRecordFlg = false; //是否正在录像
+    private MediaPlayer mMediaPlayer; //视频播放功能类
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,14 +85,17 @@ public class DetectionActivity extends AppCompatActivity implements SurfaceHolde
 
     private void initView() {
         mBtnStartDetect = findViewById(R.id.btn_start_detect);
+
         mBtnStopDetect = findViewById(R.id.btn_stop_detect);
         mSurfaceViewDetect = findViewById(R.id.surface_view_detect);
+        mBtnStartRecord = findViewById(R.id.btn_start_record);
+        mBtnStartVideoPlay =  findViewById(R.id.btn_start_video_play);
         mTvDetectTime = findViewById(R.id.tv_detect_time);
         mTvActionState = findViewById(R.id.tv_action_state);
         mTvVivoState = findViewById(R.id.tv_vivo_state);
         mTvActionTips = findViewById(R.id.tv_action_tips);
 
-        //开启
+        //开启检测
         mBtnStartDetect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -85,7 +103,7 @@ public class DetectionActivity extends AppCompatActivity implements SurfaceHolde
             }
         });
 
-        //关闭
+        //停止检测
         mBtnStopDetect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -93,14 +111,165 @@ public class DetectionActivity extends AppCompatActivity implements SurfaceHolde
             }
         });
 
+        //开始录屏
+        mBtnStartRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startRecord();
+            }
+        });
+
+        //开始视频播放
+        mBtnStartVideoPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startVideoPlay();
+            }
+        });
+
         //SurfaceView添加回调
-        mSurfaceViewDetect.getHolder().addCallback(this);
+        //mSurfaceViewDetect.getHolder().addCallback(this);
+        SurfaceHolder holder = mSurfaceViewDetect.getHolder();
+        holder.addCallback(this);
+        //setType必须设置，要不出错
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
     }
+
+    //视频播放
+    private void startVideoPlay() {
+        Log.d(TAG, "bright9#startVideoPlay");
+        if(mMediaPlayer == null) {
+            mMediaPlayer = new MediaPlayer();
+        }
+        mMediaPlayer.reset();
+        Uri uri = Uri.parse(mVideoPath); //通过文件的路径String转化成Uri
+        mMediaPlayer = MediaPlayer.create(DetectionActivity.this, uri);
+        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mMediaPlayer.setDisplay(mSurfaceHolder); //视频播放？
+        try {
+            mMediaPlayer.prepare();
+        } catch (Exception e) {
+            Log.d(TAG, "bright9#startVideoPlay#e：" + e);
+            e.printStackTrace();
+        }
+        //视频播放
+        mMediaPlayer.start();
+    }
+
+    //开始录屏或结束录屏
+    private void startRecord() {
+        Log.d(TAG, "bright9#startRecord");
+        if(mMediaRecorder == null) {
+            mMediaRecorder = new MediaRecorder();
+        }
+
+        //如果不是正在录制视频，则开始录制
+        if(!mStartedRecordFlg) {
+            //mCamera.setPreviewCallback(DetectionActivity.this);
+            //打开摄像头
+            mCamera = Camera.open(Camera.CameraInfo.CAMERA_FACING_FRONT); //前置摄像头
+            if(mCamera != null) {
+                mCamera.setDisplayOrientation(90);//摄像头角度
+                mCamera.unlock(); //必须加上，不然crash
+                mMediaRecorder.setCamera(mCamera);
+            }
+                try {
+                    //这两项需要放在setOutputFormat之前
+                    mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
+                    mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+
+                    //Set output file format
+                    mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+
+                    //这两项需要放在setOutputFormat之后
+                    mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+                    mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.MPEG_4_SP);
+
+                    //mMediaRecorder.setVideoSize(640, 480); //?
+                    //mMediaRecorder.setVideoFrameRate(30);
+                    mMediaRecorder.setVideoEncodingBitRate(3 * 1024 * 1024);
+                    //mMediaRecorder.setOrientationHint(90);
+
+                    //设置记录会话的最大持续时间(毫秒)
+                    //mMediaRecorder.setMaxDuration(60 * 1000);
+                    mMediaRecorder.setPreviewDisplay(mSurfaceHolder.getSurface());
+                    //视频存储的路径
+                    mVideoPath = getSDPath();
+                    if(mVideoPath != null) {
+                        //创建文件
+                        File dir = new File(mVideoPath + "/recordvideo");
+                        if(!dir.exists()) {
+                            dir.mkdir();
+                        }
+                        mVideoPath = dir + "/" + getData() + ".mp4"; // mPath: /storage/emulated/0/recordvideo/录制时间.mp4
+                        Log.d(TAG, "bright#initView#mPath: " + mVideoPath);
+                    //如何上传到服务器？
+                    //把视频放到本地sd card的mPath路径的文件
+                    mMediaRecorder.setOutputFile(mVideoPath);
+                    mMediaRecorder.prepare();
+                    //开始录屏
+                    mMediaRecorder.start();
+                    mStartedRecordFlg = true;
+                    mBtnStartRecord.setText("Stop");
+                }
+                } catch (Exception e) {
+                    Log.d(TAG, "bright9#e：" + e);
+                    e.printStackTrace();
+                }
+        } else {
+            //如果正在录制视频,则停止录制
+            if(mStartedRecordFlg) {
+                mMediaRecorder.stop();
+                mMediaRecorder.reset();
+                mMediaRecorder.release();
+                mMediaRecorder = null;
+                mBtnStartRecord.setText("Start");
+            }
+            mStartedRecordFlg = false;
+        }
+
+    }
+
+    /**
+     * 获取SD path
+     *
+     * */
+    public String getSDPath() {
+        File sdDir = null;
+        //获取sd卡是否存在
+        boolean sdCardExist = Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED); //sdcard
+        if(sdCardExist) {
+            //获取根目录
+            sdDir = Environment.getExternalStorageDirectory(); //sdDir: /storage/emulated/0
+            Log.d(TAG, "bright9#sdDir:" + sdDir.toString());
+            return sdDir.toString();
+        }
+        return null;
+    }
+
+    /**
+     * 获取系统时间
+     */
+    public static String getData() {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DATE);
+        int minute = calendar.get(Calendar.MINUTE);
+        int hour = calendar.get(Calendar.HOUR);
+        int second = calendar.get(Calendar.SECOND);
+
+        String date = "" + year + (month + 1) + day + hour + minute + second;
+        Log.d(TAG, "bright#date:" + date);
+        return date;
+    }
+
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
-        Log.d(TAG, "bright#surfaceCreated");
+        mSurfaceHolder = surfaceHolder;
+        Log.d(TAG, "bright9#surfaceCreated");
         callCamera(mSurfaceViewDetect.getHolder());
     }
 
@@ -118,7 +287,7 @@ public class DetectionActivity extends AppCompatActivity implements SurfaceHolde
             mCamera.startPreview();
         } catch (IOException e) {
             e.printStackTrace();
-            Log.d(TAG, "bright#Error starting camera preview: " + e.getMessage());
+            Log.d(TAG, "bright9#Error starting camera preview: " + e.getMessage());
         }
     }
 
@@ -197,7 +366,7 @@ public class DetectionActivity extends AppCompatActivity implements SurfaceHolde
 
     //初始化Camera
     private void initCamera(SurfaceHolder surfaceHolder) {
-        Log.d(TAG, "bright#initCamera");
+        Log.d(TAG, "bright9#initCamera");
         mCamera = Camera.open(1);
         //获取窗口的管理器
         WindowManager windowManager = ((WindowManager) getSystemService(Context.WINDOW_SERVICE));
