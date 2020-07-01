6fad7580fcdc793ca81903740f3df15a4f02c9a4
brightli <287055586@qq.com>
2020-07-01 08:03:47 +0800

    Activity透明设置

--- functionfacedetect/src/main/java/com/xiaoming/functionvideorecordingandfacerecognition/MainActivity.java
+++ functionfacedetect/src/main/java/com/xiaoming/functionvideorecordingandfacerecognition/MainActivity.java
@@ -13,6 +13,7 @@ import com.xiaoming.functionvideorecordingandfacerecognition.vivodetection.VivoD
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private Button mBtnRecordVideo;
     private Button mBtnVivoDetect;
+    private Button mBtTransparentActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +26,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void initView() {
         mBtnRecordVideo = findViewById(R.id.btn_record_video);
         mBtnVivoDetect = findViewById(R.id.btn_vivo_detect);
+        mBtTransparentActivity = findViewById(R.id.btn_transparent_activity);
 
         mBtnRecordVideo.setOnClickListener(this);
         mBtnVivoDetect.setOnClickListener(this);
+        mBtTransparentActivity.setOnClickListener(this);
     }
 
     @Override
@@ -38,6 +41,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.btn_vivo_detect:
                 startActivity(new Intent(MainActivity.this, VivoDetectActivity.class));
+                break;
+            case R.id.btn_transparent_activity:
+                startActivity(new Intent(MainActivity.this, TransparentActivity.class));
                 break;
         }
     }
---
+++ functionfacedetect/src/main/java/com/xiaoming/functionvideorecordingandfacerecognition/TransparentActivity.java
@@ -0,0 +1,14 @@
+package com.xiaoming.functionvideorecordingandfacerecognition;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TransparentActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transparent);
+    }
+}
