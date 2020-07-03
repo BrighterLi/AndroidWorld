a1d95ac27edb8074a4b2d35eaa2dbbe5cb9e9cf0
brightli <brightli@lexin.com>
2020-07-03 20:13:55 +0800

    人脸检测算法以so库形式提供

---
+++ functionfacedetect/src/main/java/com/xiaoming/functionvideorecordingandfacerecognition/facedetectbysolib/FaceDetectBySoLibActivity.java
@@ -0,0 +1,15 @@
+package com.xiaoming.functionvideorecordingandfacerecognition.facedetectbysolib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xiaoming.functionvideorecordingandfacerecognition.R;
+
+public class FaceDetectBySoLibActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_face_detect_by_so_lib);
+    }
+}
