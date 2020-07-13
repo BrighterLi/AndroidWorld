083912e978ec9b684015954d23eca7cb91666b47
brightli <287055586@qq.com>
2020-07-13 08:02:33 +0800

    addview

---
+++ functionfacedetect/src/main/java/com/xiaoming/functionvideorecordingandfacerecognition/addview/AddViewActivity.java
@@ -0,0 +1,15 @@
+package com.xiaoming.functionvideorecordingandfacerecognition.addview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xiaoming.functionvideorecordingandfacerecognition.R;
+
+public class AddViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_view);
+    }
+}
