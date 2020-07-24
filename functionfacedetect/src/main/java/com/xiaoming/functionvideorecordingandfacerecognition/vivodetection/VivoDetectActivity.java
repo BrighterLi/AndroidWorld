0e27e851af07ec7b986f5c1f7f61909998fa56cd
brightli <287055586@qq.com>
2020-07-24 08:14:04 +0800

    人脸活体检测 添加模型文件

--- functionfacedetect/src/main/java/com/xiaoming/functionvideorecordingandfacerecognition/vivodetection/VivoDetectActivity.java
+++ functionfacedetect/src/main/java/com/xiaoming/functionvideorecordingandfacerecognition/vivodetection/VivoDetectActivity.java
@@ -1,18 +1,29 @@
 package com.xiaoming.functionvideorecordingandfacerecognition.vivodetection;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.xiaoming.functionvideorecordingandfacerecognition.R;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.List;
 
 import pub.devrel.easypermissions.EasyPermissions;
 
 //https://github.com/zeusees/Zeusee-Face-Anti-spoofing
-public class VivoDetectActivity extends AppCompatActivity {
+public class VivoDetectActivity extends Activity implements EasyPermissions.PermissionCallbacks{
     private Button mBtnVivoDetect;
     private int RC_READ_EXTERNAL_STORAGE = 2;
 
@@ -39,11 +50,12 @@ public class VivoDetectActivity extends AppCompatActivity {
                     Manifest.permission.CAMERA,
                     Manifest.permission.RECORD_AUDIO //音频录制
             );
-        }
+        } else {
+                //拷贝模型文件
+                initRecognizer();
+            }
 
         initView();
-
-
     }
 
     private void initView() {
@@ -57,4 +69,58 @@ public class VivoDetectActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+        initRecognizer();
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+        Toast.makeText(this, "禁止", Toast.LENGTH_SHORT).show();
+    }
+
+    public void initRecognizer() {
+
+        String assetPath = "AliveDetection";
+        String sdcardPath = Environment.getExternalStorageDirectory()
+                + File.separator + assetPath;
+        copyFilesFromAssets(this, assetPath, sdcardPath);
+
+    }
+
+    public void copyFilesFromAssets(Context context, String oldPath, String newPath) {
+        try {
+            String[] fileNames = context.getAssets().list(oldPath);
+            if (fileNames.length > 0) {
+                // directory
+                File file = new File(newPath);
+                if (!file.mkdirs()) {
+                    Log.d("mkdir", "can't make folder");
+
+                }
+//                    return false;                // copy recursively
+                for (String fileName : fileNames) {
+                    copyFilesFromAssets(context, oldPath + "/" + fileName,
+                            newPath + "/" + fileName);
+                }
+            } else {
+                // file
+                InputStream is = context.getAssets().open(oldPath);
+                FileOutputStream fos = new FileOutputStream(new File(newPath));
+                byte[] buffer = new byte[1024];
+                int byteCount;
+                while ((byteCount = is.read(buffer)) != -1) {
+                    fos.write(buffer, 0, byteCount);
+                }
+                fos.flush();
+                is.close();
+                fos.close();
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
 }
