1cc6151ebfdc648c3a9719ccc858a959607a4aed
brightli <287055586@qq.com>
2020-08-06 08:23:41 +0800

    Theme 设置

---
+++ androidknowledgepoints/src/main/java/com/xiaoming/androidknowledgepoints/Theme/SetThemeActivity.java
@@ -0,0 +1,15 @@
+package com.xiaoming.androidknowledgepoints.Theme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xiaoming.androidknowledgepoints.R;
+
+public class SetThemeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_theme);
+    }
+}
