8b0ba94bb084935c8aa99ab66fd9a47d9fc7d86e
brightli <287055586@qq.com>
2020-08-20 08:12:29 +0800

    Scheme

---
+++ androidknowledgepoints/src/main/java/com/xiaoming/androidknowledgepoints/scheme/SchemeActivity.java
@@ -0,0 +1,15 @@
+package com.xiaoming.androidknowledgepoints.scheme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.xiaoming.androidknowledgepoints.R;
+
+public class SchemeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scheme);
+    }
+}
