1c99d64c19b2826092b1b7771a396c971c89b4bf
brightli <brightli@lexin.com>
2020-09-23 22:21:22 +0800

    ScrollView+TabLayout+ViewPager+RecyclerView

--- androidknowledgepoints/src/main/java/com/xiaoming/androidknowledgepoints/uiframe/EnterUiFrameActivity.java
+++ androidknowledgepoints/src/main/java/com/xiaoming/androidknowledgepoints/uiframe/EnterUiFrameActivity.java
@@ -2,19 +2,36 @@ package com.xiaoming.androidknowledgepoints.uiframe;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.xiaoming.androidknowledgepoints.R;
+import com.xiaoming.androidknowledgepoints.uiframe.ScrollView.UiFrame2Activity;
+import com.xiaoming.androidknowledgepoints.uiframe.ScrollView1TabLayout1ViewPager1RecyclerView.UiFrame3Activity;
 import com.xiaoming.androidknowledgepoints.uiframe.recycleview.UiFrame1Activity;
 
 public class EnterUiFrameActivity extends Activity implements View.OnClickListener{
+    private Button mBtRecyclerview;
+    private Button mScrollview;
+    private Button mScrollviewTablayoutFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_ui_frame);
+
+        initView();
+    }
+
+    private void initView() {
+        mBtRecyclerview = findViewById(R.id.bt_recyclerview);
+        mScrollview = findViewById(R.id.bt_scrollview);
+        mScrollviewTablayoutFragment = findViewById(R.id.bt_scrollview_tablayout_fragment);
+
+        mBtRecyclerview.setOnClickListener(this);
+        mScrollview.setOnClickListener(this);
+        mScrollviewTablayoutFragment.setOnClickListener(this);
     }
 
     @Override
@@ -22,6 +39,12 @@ public class EnterUiFrameActivity extends Activity implements View.OnClickListen
         switch (v.getId()) {
             case R.id.bt_recyclerview:
                 startActivity(new Intent(EnterUiFrameActivity.this, UiFrame1Activity.class));
+                break;
+            case R.id.bt_scrollview:
+                startActivity(new Intent(EnterUiFrameActivity.this, UiFrame2Activity.class));
+                break;
+            case R.id.bt_scrollview_tablayout_fragment:
+                startActivity(new Intent(EnterUiFrameActivity.this, UiFrame3Activity.class));
                 break;
         }
     }
---
+++ androidknowledgepoints/src/main/java/com/xiaoming/androidknowledgepoints/uiframe/ScrollView1TabLayout1ViewPager1RecyclerView/ThreeFragment.java
@@ -0,0 +1,43 @@
+package com.xiaoming.androidknowledgepoints.uiframe.ScrollView1TabLayout1ViewPager1RecyclerView;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.xiaoming.androidknowledgepoints.R;
+
+import java.util.ArrayList;
+
+
+public class ThreeFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerViewAdapter mAdapter;
+    private View mainView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mainView=inflater.inflate(R.layout.fragment_three,container,false);
+        initView();
+        return mainView;
+    }
+
+    private void initView(){
+        mRecyclerView=mainView.findViewById(R.id.recyclerview);
+        mLayoutManager=new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        ArrayList<String> data=new ArrayList<>();
+        for(int i=0;i<20;i++){
+            data.add("列表项"+i);
+        }
+        mAdapter=new RecyclerViewAdapter(getActivity(),data);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+}
