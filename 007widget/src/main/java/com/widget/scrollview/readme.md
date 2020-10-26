52740669279e67e38c93bfe17c1cf3253681a9c7
brightli <287055586@qq.com>
2020-10-26 08:41:02 +0800

    Scrollview

--- 007widget/src/main/java/com/widget/scrollview/readme.md
+++ 007widget/src/main/java/com/widget/scrollview/readme.md
@@ -1,3 +1,6 @@
-1 scrollview
-2 viewpager
-3 tablayout
+1 ScrollView
+2 ViewPager
+3 TabLayout
+(1) 点击前后文字颜色和粗细变化
+android tablayout选中的字体颜色及大小改变:https://www.jianshu.com/p/9cd9d604ce46
+(2) 下滑线颜色和宽度高度距离形状自定义
---
+++ 008project/src/main/java/com/xiaoming/a008project/consumption/ConsumptionActivity.java
@@ -0,0 +1,15 @@
+package com.xiaoming.a008project.consumption;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xiaoming.a008project.R;
+
+public class ConsumptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consumption);
+    }
+}
