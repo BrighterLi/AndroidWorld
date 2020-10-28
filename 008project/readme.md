c0aed0791d29ca8ef367906254214257aea931ed
brightli <brightli@lexin.com>
2020-10-28 17:29:34 +0800

    DesktopNewsReddot

---
+++ 008project/readme.md
@@ -0,0 +1,5 @@
+1 消费号
+2 活体检测与视屏录制
+分期乐活体检测和录制流程介绍：http://wiki.fenqile.com/pages/viewpage.action?pageId=48506952
+3 App故障诊断
+App故障诊断方案 ：http://wiki.fenqile.com/pages/viewpage.action?pageId=45326679
---
+++ 008project/src/main/java/com/xiaoming/a008project/smallproject/desktopnewsreddot/TestActivity.java
@@ -0,0 +1,15 @@
+package com.xiaoming.a008project.smallproject.desktopnewsreddot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xiaoming.a008project.R;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
