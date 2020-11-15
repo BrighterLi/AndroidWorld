a6561b3ee0e167e37886d8cbef70598526c37032
brightli <287055586@qq.com>
2020-11-15 23:24:52 +0800

    cookie

---
+++ 002net/src/main/java/com/xiaoming/net/cookie/CookieTestActivity.java
@@ -0,0 +1,17 @@
+package com.xiaoming.net.cookie;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xiaoming.net.R;
+
+//Android下对Cookie的读写操作（附Demo）:https://blog.csdn.net/lishuai05251986/article/details/84804199
+public class CookieTestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cookie_test);
+    }
+}
