26c1885ecd4d8cf1d7a04f1149e2fe105c48bb33
brightli <287055586@qq.com>
2021-01-14 00:16:28 +0800

    链表翻转

---
+++ 000algorithm/src/main/java/com/xiaoming/a005algorithm/datastructure/linkedlist/LinkedList.java
@@ -0,0 +1,23 @@
+package com.xiaoming.a005algorithm.datastructure.linkedlist;
+
+public class LinkedList {
+
+    //反转链表，输出链表后的头部
+    public ListNode reverseList(ListNode head) {
+        ListNode preNode = null; //前一个节点
+        ListNode nextNode = null; //后一个节点
+        ListNode curNode = head; //当前节点
+        if(head == null) {
+            return null; //记得判空
+        } else if(head.next == null) {
+            return head; //记得特殊情况拿出来
+        }
+        while(curNode != null) {
+            nextNode = curNode.next; //保存下一个节点
+            curNode.next = preNode; //反转指针指向
+            preNode = curNode; //保存并更新前面一个节点
+            curNode = nextNode;  //更新当前
+        }
+        return preNode;
+    }
+}
