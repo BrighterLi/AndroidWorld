47283fbf01bbb131826c2c1086c1c7ee5c972cc4
brightli <287055586@qq.com>
2021-06-25 09:23:41 +0800

    Activity动画

--- 007widget/src/main/java/com/widget/animation/activity/Activity1.java
+++ 007widget/src/main/java/com/widget/animation/activity/Activity1.java
@@ -17,11 +17,14 @@ public class Activity1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
-        overridePendingTransition(R.anim.popwindow_no_anim, R.anim.popwindow_out_from_bottom_to_top);
+        //overridePendingTransition(R.anim.popwindow_no_anim, R.anim.popwindow_out_from_bottom_to_top);
+        //overridePendingTransition(R.anim.popwindow_in, R.anim.popwindow_out_from_bottom_to_top);
+        overridePendingTransition(0, 0);
         mBt = findViewById(R.id.bt_jump);
         mBt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                overridePendingTransition(R.anim.popwindow_in, R.anim.popwindow_no_anim);
                 startActivity(new Intent(Activity1.this, Activity2.class));
             }
         });
--- 007widget/src/main/java/com/widget/animation/activity/Activity2.java
+++ 007widget/src/main/java/com/widget/animation/activity/Activity2.java
@@ -16,7 +16,9 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
-        overridePendingTransition(R.anim.popwindow_no_anim, R.anim.popwindow_out);
+        //overridePendingTransition(R.anim.popwindow_no_anim, R.anim.popwindow_out_from_top_to_bottom);
+        //overridePendingTransition(R.anim.popwindow_in, R.anim.popwindow_out_from_top_to_bottom);
+        overridePendingTransition(R.anim.popwindow_no_anim, R.anim.popwindow_out_from_bottom_to_top);
 
         mBtBack = findViewById(R.id.bt_finish);
         mBtBack.setOnClickListener(new View.OnClickListener() {
