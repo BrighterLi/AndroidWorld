e0ea219711679b01dcc2b4e3bbb7f55424bdf2c3
brightli <brightli@lexin.com>
2021-06-30 22:20:58 +0800

    RecyclerView  GridLayoutManager 4列

---
+++ 008project/src/main/java/com/xiaoming/a008project/fenle/test/recyclerview/RecyclerViewDemoActivity.java
@@ -0,0 +1,98 @@
+package com.xiaoming.a008project.fenle.test.recyclerview;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import com.xiaoming.a008project.R;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecyclerViewDemoActivity extends AppCompatActivity {
+    //@BindView(R.id.recyclerView)
+    RecyclerView mRecyclerView;
+    private List<String> datas = new ArrayList<>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view_demo);
+        //ButterKnife.bind(this);
+        mRecyclerView = findViewById(R.id.recyclerView);
+
+        initView();
+    }
+
+    private void initView() {
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 4);
+        mRecyclerView.setLayoutManager(gridLayoutManager);
+
+        for (int i = 0; i < 100; i++) {
+            datas.add("测试数据  " + i);
+        }
+        DataAdapter dataAdapter = new DataAdapter(this, datas);
+        mRecyclerView.setAdapter(dataAdapter);
+
+    }
+
+
+    class DataAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+        private Context mContext;
+        private List<String> mDatas;
+        private LayoutInflater mLayoutInflater;
+
+        private DataAdapter(Context context, List<String> datas) {
+            mContext = context;
+            mDatas = datas;
+            mLayoutInflater = LayoutInflater.from(context);
+        }
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            RecyclerView.ViewHolder viewHolder = new ViewHolder(mLayoutInflater.inflate(R.layout.item_data, parent, false));
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+            ViewHolder viewHolder = (ViewHolder) holder;
+            viewHolder.bindPosition(position);
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mDatas.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            //@BindView(R.id.tv_content)
+            private TextView mTvContent;
+            public ViewHolder(View itemView) {
+                super(itemView);
+                //ButterKnife.bind(this, itemView);
+                mTvContent = itemView.findViewById(R.id.tv_content);
+            }
+
+            public void bindPosition(int position) {
+                String title = mDatas.get(position);
+                mTvContent.setText(title);
+            }
+        }
+    }
+
+}
