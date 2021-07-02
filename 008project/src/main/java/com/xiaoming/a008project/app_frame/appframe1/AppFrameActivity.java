670ffa98e843ea570b02724ed2d91cdf61e5bc21
brightli <287055586@qq.com>
2021-07-03 02:55:05 +0800

    app框架

---
+++ 008project/src/main/java/com/xiaoming/a008project/app_frame/appframe1/AppFrameActivity.java
@@ -0,0 +1,16 @@
+package com.xiaoming.a008project.app_frame.appframe1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.xiaoming.a008project.R;
+
+public class AppFrameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_frame);
+    }
+}
