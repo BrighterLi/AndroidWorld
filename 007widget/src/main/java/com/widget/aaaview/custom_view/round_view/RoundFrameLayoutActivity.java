b2432d17a114f44b1993c04b0a98a4a2e6b48df6
brightli <287055586@qq.com>
2021-07-04 10:48:27 +0800

    自定义View: 圆角矩形框背景

---
+++ 007widget/src/main/java/com/widget/aaaview/custom_view/round_view/RoundFrameLayoutActivity.java
@@ -0,0 +1,16 @@
+package com.widget.aaaview.custom_view.round_view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.widget.R;
+
+public class RoundFrameLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_round_frame_layout);
+    }
+}
