13f818f5e98d1728291fcf693d7d5cef3b0eb055
brightli <287055586@qq.com>
2021-07-26 09:00:27 +0800

    Kotlin 工具类

---
+++ 010kotlin/src/main/java/com/xiaoming/a010kotlin/xiangxuedemo/modules/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.xiaoming.a010kotlin.xiangxuedemo.modules.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.xiaoming.a010kotlin.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ 010kotlin/src/main/java/com/xiaoming/a010kotlin/xiangxuedemo/modules/login/interfaces/LoginView.kt
@@ -0,0 +1,8 @@
+package com.xiaoming.a010kotlin.xiangxuedemo.modules.login.interfaces
+
+//View层
+interface LoginView {
+    //把结果显示到Activity/Fragment
+    //fun loginSuccess(loginBean: LoginResponse ?)
+    //fun loginFailure(errorMsg: String ?)
+}
