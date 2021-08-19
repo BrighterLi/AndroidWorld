ec4eb3705d0b12e3be0cd69b04fe89dde4f33d65
brightli <287055586@qq.com>
2021-08-19 09:11:53 +0800

    databinding框架

--- 009frame/src/main/java/com/xiaoming/databinding/DataBindingActivity.java
+++ 009frame/src/main/java/com/xiaoming/databinding/DataBindingActivity.java
@@ -1,12 +1,15 @@
 package com.xiaoming.databinding;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.app.Activity;
 import android.os.Bundle;
 
 import com.xiaoming.framearouter.R;
+import com.xiaoming.framearouter.databinding.ActivityDataBindingBinding;
 
+//Android官方数据绑定框架DataBinding用法详解+附带DEMO源码: https://blog.csdn.net/tyhj_sf/article/details/52065093
 public class DataBindingActivity extends AppCompatActivity {
 
     @Override
