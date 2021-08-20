9dcd07a3024bf94f9a28222158aa31a50a170910
brightli <287055586@qq.com>
2021-08-20 09:07:32 +0800

    mvvm

---
+++ 009frame/src/main/java/com/xiaoming/mvx/mvvm/demo1/MvvmActivity.java
@@ -0,0 +1,17 @@
+package com.xiaoming.mvx.mvvm.demo1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.xiaoming.framearouter.R;
+
+//Android MVVM实战Demo完全解析: https://blog.csdn.net/zhouxu88/article/details/78284198
+public class MvvmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mvvm);
+    }
+}
