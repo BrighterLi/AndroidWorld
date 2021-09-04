b78c8cc4b3d64e7eea1e1d3d18dac2d9c37a7697
brightli <287055586@qq.com>
2021-09-04 16:31:08 +0800

    aop

---
+++ 000androidpoints/src/main/java/com/xiaoming/androidpoints/aaajavaknowledge/aop/aspectj/AspectJDemoActivity.java
@@ -0,0 +1,16 @@
+package com.xiaoming.androidpoints.aaajavaknowledge.aop.aspectj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.xiaoming.androidpoints.R;
+
+public class AspectJDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aspect_jdemo);
+    }
+}
