d9322b05a1f30a962241d4ce26a2c7b493ff1ea8
brightli <287055586@qq.com>
2021-09-17 09:25:03 +0800

    RxJava+Retrofit

---
+++ 002net/src/main/java/com/xiaoming/net/frame/rxjava/okhttp_retrofit_rxjava/RxJavaRetrofitActivity.java
@@ -0,0 +1,16 @@
+package com.xiaoming.net.frame.rxjava.okhttp_retrofit_rxjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.xiaoming.net.R;
+
+public class RxJavaRetrofitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rx_java_retrofit);
+    }
+}
