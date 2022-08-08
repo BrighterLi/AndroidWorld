636e59b6249460327e08af8472f80974c22b5d40
brightli <287055586@qq.com>
2022-08-09 07:24:29 +0800

    聊天页面

---
+++ 010kotlin/src/main/java/com/xiaoming/a010kotlin/project/quick_chat/demo2/QuickChatActivity2.kt
@@ -0,0 +1,13 @@
+package com.xiaoming.a010kotlin.project.quick_chat.demo2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.xiaoming.a010kotlin.R
+
+class QuickChatActivity2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quick_chat2)
+    }
+}
