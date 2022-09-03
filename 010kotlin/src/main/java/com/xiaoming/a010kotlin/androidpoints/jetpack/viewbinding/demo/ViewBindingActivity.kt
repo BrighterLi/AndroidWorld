22c559d30ad8234bd12b4bf3c0a5e2b6bfb830d9
brightli <287055586@qq.com>
2022-09-03 22:57:38 +0800

    viewBinding

--- 010kotlin/src/main/java/com/xiaoming/a010kotlin/androidpoints/jetpack/viewbinding/demo/ViewBindingActivity.kt
+++ 010kotlin/src/main/java/com/xiaoming/a010kotlin/androidpoints/jetpack/viewbinding/demo/ViewBindingActivity.kt
@@ -2,13 +2,25 @@ package com.xiaoming.a010kotlin.androidpoints.jetpack.viewbinding.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.xiaoming.a010kotlin.R
+import com.xiaoming.a010kotlin.databinding.ActivityViewBindingBinding
 
 class ViewBindingActivity : AppCompatActivity() {
+    companion object {
+        private const val TAG = "ViewBindingActivity"
+    }
+    private lateinit var viewBinding: ActivityViewBindingBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_binding)
+        viewBinding = ActivityViewBindingBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        viewBinding.tvViewBinding.text = "viewBinding test"
+        viewBinding.btViewBinding.setOnClickListener {
+            Log.d(TAG, "click")
+        }
 
     }
 }
