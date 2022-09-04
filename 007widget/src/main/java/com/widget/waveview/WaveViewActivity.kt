035ce1d98a4020c6b5018cf8c6bc7ba696630600
brightli <287055586@qq.com>
2022-09-05 00:32:03 +0800

    录音音频动效

---
+++ 007widget/src/main/java/com/widget/waveview/WaveViewActivity.kt
@@ -0,0 +1,12 @@
+package com.widget.waveview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.widget.R
+
+class WaveViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wave_view)
+    }
+}
