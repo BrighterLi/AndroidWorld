d43d812a980e81efb81aa9326929322ed8758d54
brightli <287055586@qq.com>
2022-09-18 23:36:34 +0800

    FileProvider

---
+++ 000androidpoints/src/main/java/com/xiaoming/androidpoints/fileprovider/FileProviderActivity.kt
@@ -0,0 +1,12 @@
+package com.xiaoming.androidpoints.fileprovider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.xiaoming.androidpoints.R
+
+class FileProviderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_file_provider)
+    }
+}
