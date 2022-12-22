e656d44c9bae1fd5d194a5c8232796a300ee2332
brightli <287055586@qq.com>
2022-12-23 00:36:39 +0800

    RecyelrView 高度变化&内容滑动

--- 007widget/src/main/java/com/widget/recyclerview/scroll/RvScrollControlActivity.kt
+++ 007widget/src/main/java/com/widget/recyclerview/scroll/RvScrollControlActivity.kt
@@ -2,9 +2,10 @@ package com.widget.recyclerview.scroll
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.widget.R
 import com.widget.databinding.ActivityScrollBinding
 import com.widget.recyclerview.heightchange.RvAdapter
 
@@ -46,19 +47,57 @@ class RvScrollControlActivity : AppCompatActivity() {
         msgList.add("555555555555555555")
         msgList.add("66666666666666666666666")
         msgList.add("7777777777777777777777777")
-        msgList.add(
+        /*msgList.add(
             "8888888888888888888888888888ddddddddfffffffffffffffffffffffeeeeeeeeeeeeeeeee" +
                     "eeeeeeeeettttttttttttttttttttttttttttttttttttqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq" +
                     "qqqqqqqqqwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwweeeeeeeeeeee" +
                     "eeeeeeeeeeeeeeeeeeeeeerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrryyyyyyyyyyyyyyyyyyyyyyyyyyyy" +
                     "yyyyyygggggggggggggggggggggggg"
         )
+        msgList.add("1111111111111")
+        msgList.add("22222222222222")
+        msgList.add("333333333333333")
+        msgList.add("4444444444444444444")
+        msgList.add("555555555555555555")
+        msgList.add("66666666666666666666666")
+        msgList.add("7777777777777777777777777")
+        msgList.add("8888888888888888888888888888")
+        msgList.add("9999999999999999999999999999999999")
+        msgList.add("111111111111111111111111111111111")
+        msgList.add("1111111111111")
+        msgList.add("22222222222222")
+        msgList.add("333333333333333")
+        msgList.add("4444444444444444444")
+        msgList.add("555555555555555555")
+        msgList.add("66666666666666666666666")
+        msgList.add("7777777777777777777777777")
+        msgList.add("1111111111111")
+        msgList.add("22222222222222")
+        msgList.add("333333333333333")
+        msgList.add("4444444444444444444")
+        msgList.add("555555555555555555")
+        msgList.add("66666666666666666666666")
+        msgList.add("7777777777777777777777777")
+        msgList.add("8888888888888888888888888888")
+        msgList.add("9999999999999999999999999999999999")
+        msgList.add("111111111111111111111111111111111")
+        msgList.add("1111111111111")
+        msgList.add("22222222222222")
+        msgList.add("333333333333333")
+        msgList.add("4444444444444444444")
+        msgList.add("555555555555555555")
+        msgList.add("66666666666666666666666")
+        msgList.add("7777777777777777777777777")*/
     }
 
     private fun initView() {
         initMsgRvView()
-        smoothScrollMsgRvToBottom()
+
         //scrollMsgRvToBottom()
+        viewBinding.button.setOnClickListener {
+            changeViewHeight(viewBinding.button)
+            smoothScrollMsgRvToBottom()
+        }
     }
 
     private fun initMsgRvView() {
@@ -74,5 +113,16 @@ class RvScrollControlActivity : AppCompatActivity() {
 
     private fun smoothScrollMsgRvToBottom() {
         viewBinding.rvScroll.smoothScrollToPosition(msgListAdapter.data.size - 1)
+    }
+
+    private fun changeViewHeight(view: View) {
+        val linearParams: ViewGroup.LayoutParams = view.layoutParams
+        linearParams.height = dip2px(200f)
+        view.layoutParams = linearParams
+    }
+
+    private fun dip2px(value: Float): Int {
+        val scale = resources.displayMetrics.density
+        return (scale * value + 0.5f).toInt()
     }
 }
