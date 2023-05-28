e3eb2bec22e55a85cec389f13b1111866380a64b
brightli <287055586@qq.com>
2023-05-28 14:56:29 +0800

    Android.mk编译通过不了 暂时屏蔽

--- 000androidpoints/src/main/jni/Android.mk
+++ 000androidpoints/src/main/jni/Android.mk
@@ -1,14 +1,14 @@
-LOCAL_PATH := $(call my-dir)
+#LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
+#include $(CLEAR_VARS)
 
 # 定义生成so文件的名字
-LOCAL_MODULE    := androidknowledgepoints
+#LOCAL_MODULE    := androidknowledgepoints
 # 需要编译的文件，由于在一个文件夹下，不用写路径
-LOCAL_SRC_FILES := jnitools.c
+#LOCAL_SRC_FILES := jnitools.c
 
-LOCAL_LDLIBS :=  -L$(SYSROOT)/usr/lib -llog
+#LOCAL_LDLIBS :=  -L$(SYSROOT)/usr/lib -llog
 
-include $(BUILD_SHARED_LIBRARY)
+#include $(BUILD_SHARED_LIBRARY)
 # 指定生成全部的ABI的so文件
-APP_ABI := all
+#APP_ABI := all
