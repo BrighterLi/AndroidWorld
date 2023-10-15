20393d7cad4bfa870ad9f4eaa14dd03a1b34f5cf
BrighterLi <287055586@qq.com>
2023-10-16 00:21:36 +0800

    TextView 超过3行后，设置最多显示三行，并且字体由16变为14

--- 007widget/src/main/java/com/widget/textview/TextView1Activity.java
+++ 007widget/src/main/java/com/widget/textview/TextView1Activity.java
@@ -2,14 +2,37 @@ package com.widget.textview;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ViewTreeObserver;
+import android.widget.TextView;
 
 import com.widget.R;
 
 public class TextView1Activity extends AppCompatActivity {
+    private TextView textView3;
+    private static String TAG = "TextView1Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_view1);
+
+        textView3 = findViewById(R.id.textView3);
+        String text = textView3.getText().toString();
+        textView3.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                int length = text.length();
+                Character character = text.charAt(length-1);
+                int lineEnd = textView3.getLayout().getLineEnd(2);
+                Log.d(TAG, "onGlobalLayout text: " + text + "  lineEnd: "  + lineEnd + "  character: " + character
+                + "  maxline: " + textView3.getMaxLines() + "  length: " + length);
+                if (lineEnd < length && lineEnd > 0) {
+                    textView3.setMaxLines(3);
+                    textView3.setTextSize(14);
+                }
+            }
+        });
+
     }
 }
